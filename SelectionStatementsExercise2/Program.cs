417744fa91d981cab95cfda7f9404b5f6eb72891
UNICK J <jeffreyodeloui@gmail.com>
2025-05-04 14:51:51 -0400

    Completed the School subject exercice

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Threading.Channels;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Heyyy what's up !!? What was your favorite subject at school ?");
+            string favSchoolSubject = Console.ReadLine()?.Trim().ToLower();
+
+
+            switch(favSchoolSubject)
+            {
+                case "math" :
+                Console.WriteLine("Good choice. Math is a very interesting subject.");
+                break;
+
+                case "physics" :
+                Console.WriteLine("Physics is so wide and the applications so diverse. I use to love it.");
+                break;
+
+                case "biology" :
+                Console.WriteLine(" Humm Biology my second favorite subject. Love it !!");
+                break;
+
+                case "english" :
+                Console.WriteLine("Oooh no English !? Really ? It use to be so boring to me. " +
+                                  "But it's ok ");
+                break;
+
+                case "history" :
+                Console.WriteLine(" Who do not like History ? It's literally the most interesting subject.");
+                break;
+
+                default:
+                Console.WriteLine($"{favSchoolSubject} is definitely a good subject. What do you like about it ?");
+                break;
+            }
+            
+        
+
         }
     }
 }
